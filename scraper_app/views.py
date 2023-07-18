654a0944d1fd8eda90bedfae206d2abde6118fd3
pminesh <pminesh470@gmail.com>
2023-07-18 15:55:49 +0530

    add docstring

--- scraper_app/views.py
+++ scraper_app/views.py
@@ -7,12 +7,21 @@ from scraper_app import models as scraper_models
 from scraper_app.serializers import AppDetailsSerializer
 
 class GoogleAppStoreView(APIView):
+    """
+    API view to fetch and store Google Play Store app details.
+    """
     def get(self, request):
+        """
+        GET request handler to fetch all Google Play Store app details.
+        """
         all_apps_data = scraper_models.AppDetails.objects.all()
         serializer = AppDetailsSerializer(all_apps_data,many=True)
         return Response({"google_apps_data":serializer.data},status=status.HTTP_200_OK)
     
     def post(self, request):
+        """
+        POST request handler to initiate scraping of Google Play Store app details.
+        """
         url = request.data.get("url")
         lang = request.data.get("lang")
         country = request.data.get("country")
